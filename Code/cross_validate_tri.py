4fa57d1a1dfc8eeb57753d0bd6b806f5c1634d85
unknown <piyushbora1999@gmail.com>
2023-12-28 01:36:40 +0530

    Added supplementary information, one hot encoding

--- Code/cross_validate_tri.py
+++ Code/cross_validate_tri.py
@@ -20,9 +20,9 @@ class model_evaluate:
         self.features = features
         self.target = target
 
-    def cross_val_study(self, model):
+    def cross_val_study(self, model, repeats):
         scorer = make_scorer(roc_auc_score, needs_proba=True, multi_class='ovr')
-        kf = RepeatedKFold(n_repeats=20)
+        kf = RepeatedKFold(n_repeats=repeats)
         metrics = ['accuracy', 'precision', 'recall', 'f1', 'roc-auc']
         scoring_dict = {
             'roc-auc':scorer, 'f1':make_scorer(f1_score, average='macro'), 
@@ -35,32 +35,35 @@ class model_evaluate:
         metric_scores = np.array([np.mean(cv_score['test_'+metric]) for metric in metrics])
         return np.round(metric_scores, 2)
 
+def main():
+    data_ = pd.read_csv('Data/final_data.csv', index_col=0)
+    target = data_['Gender3']
+    rfc = RandomForestClassifier()
+    abc = AdaBoostClassifier()
+    gbd = GradientBoostingClassifier()
+    evc = VotingClassifier([('rfc', rfc), ('abc', abc), ('gbd', gbd)], voting='soft')
+    svc = svm.SVC(probability=True)
+    evaluations = []
+    phonology = [2, 3, 4, 5, 6, 7]
+    morphology = [8, 9, 10, 11, 12]
+    semantic = [13, 14, 15]
+    features_list = [(phonology, 'phonology'), (morphology, 'morphology'), (semantic, 'semantic'),
+                    (phonology+morphology, 'phon+morph'), (phonology+semantic, 'phon+sem'),
+                    (morphology+semantic, 'morph+sem'), (phonology+semantic+morphology, 'all')]
+    models = [(rfc, 'rfc'), (abc, 'abc'), (gbd, 'gbd'), (svc, 'svc'), (evc, 'evc')]
+    for feat in features_list:
+        features = np.array(data_[data_.columns[feat[0]]])
+        features_full = model_evaluate(features, target)
+        for model in models:
+            res = list(features_full.cross_val_study(model[0]))
+            res = [feat[1], model[1]] + res
+            evaluations.append(res)
 
-data_ = pd.read_csv('Data/final_data.csv', index_col=0)
-target = data_['Gender3']
-rfc = RandomForestClassifier()
-abc = AdaBoostClassifier()
-gbd = GradientBoostingClassifier()
-evc = VotingClassifier([('rfc', rfc), ('abc', abc), ('gbd', gbd)], voting='soft')
-svc = svm.SVC(probability=True)
-evaluations = []
-phonology = [2, 3, 4, 5, 6, 7]
-morphology = [8, 9, 10, 11, 12]
-semantic = [13, 14, 15]
-features_list = [(phonology, 'phonology'), (morphology, 'morphology'), (semantic, 'semantic'),
-                 (phonology+morphology, 'phon+morph'), (phonology+semantic, 'phon+sem'),
-                 (morphology+semantic, 'morph+sem'), (phonology+semantic+morphology, 'all')]
-models = [(rfc, 'rfc'), (abc, 'abc'), (gbd, 'gbd'), (svc, 'svc'), (evc, 'evc')]
-for feat in features_list:
-    features = np.array(data_[data_.columns[feat[0]]])
-    features_full = model_evaluate(features, target)
-    for model in models:
-        res = list(features_full.cross_val_study(model[0]))
-        res = [feat[1], model[1]] + res
-        evaluations.append(res)
+    evaluations = pd.DataFrame(evaluations,
+                            columns=['Features', 'Model', 'accuracy', 'precision',
+                                        'recall', 'f1', 'roc-auc'])
 
-evaluations = pd.DataFrame(evaluations,
-                           columns=['Features', 'Model', 'accuracy', 'precision',
-                                     'recall', 'f1', 'roc-auc'])
+    evaluations.to_csv('Results/eval3_cv_nonhp.csv')
 
-evaluations.to_csv('Results/eval3_cv_nonhp.csv')
+if(__name__=='__main__'):
+    main()
