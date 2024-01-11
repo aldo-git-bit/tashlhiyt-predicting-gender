a1a7e124a971880cf3d4eb129f8421afb6c3bf99
unknown <piyushbora1999@gmail.com>
2024-01-10 16:33:28 -0800

    ok, last commit

--- Code/gen_plot_eval.py
+++ Code/gen_plot_eval.py
@@ -7,7 +7,7 @@ different models on the different datasets. The plots generated using this are:
 import pandas as pd
 import matplotlib.pyplot as plt
 
-results = pd.read_csv('Results/supp_cv_nonhp.csv', index_col=0)
+results = pd.read_csv('Results/eval_cv_hp.csv', index_col=0)
 
 results_gr = pd.pivot_table(results, values='roc-auc', index='Features', columns='Model', sort=False)
 ax = results_gr.plot(kind='bar', rot=0, colormap='tab20b')
@@ -19,4 +19,4 @@ ax.set_xlabel("Sets of Features used for Prediction")
 ax.set_ylabel("ROC-AUC Score")
 ax.legend(ncol=len(results_gr.columns))
 ax.set_yticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-plt.savefig("Plots/plot_supp_nonhp_rocauc.png")
+plt.savefig("Plots/plot_cv_hp_rocauc.png")
